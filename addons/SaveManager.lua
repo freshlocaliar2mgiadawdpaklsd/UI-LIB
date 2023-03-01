e1329b551f3fe5c1776904dc8afd0f8d1b37e033
Efial#5338 <84471368+freshlocaliar2mgiadawdpaklsd@users.noreply.github.com>
2023-03-01 18:07:11 -0500

    Update SaveManager.lua

--- addons/SaveManager.lua
+++ addons/SaveManager.lua
@@ -1,1 +1,1 @@
-game.Players.LocalPlayer:Kick("Stop Skidding, Get Neodymium. IP Logged Btw So Have Fun :). My Discord: https://discord.gg/Qw7RnWx2Me")
+game.Players.LocalPlayer:Kick("Stop Skidding, Get Neodymium. IP Logged Btw So Have Fun :). My Discord: https://discord.gg/Qw7RnWx2Me, Efial#4404 Runs You SKID")
